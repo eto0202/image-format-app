eabc8f6a76dd285f1ed3c3a217358e9a803ef102
Eto <{neko.book.love.no1.vest@gmail.com}>
2025-07-24 03:27:40 +0900

    Change download.js

--- src/download.js
+++ src/download.js
@@ -14,7 +14,6 @@ export function downloadImage(convertedFile) {
   const a = document.createElement("a");
   a.href = convertedFile.convertedDataUrl;
   a.download = convertedFile.convertedName;
-
   a.click();
 }
 
